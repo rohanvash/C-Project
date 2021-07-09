ae4bf0eaa8fc1948bd464b4f7e322b07731f21a1
rohanvash <52627134+rohanvash@users.noreply.github.com>
2021-07-09 16:26:07 +0200

    Update Code.cs

--- Code.cs
+++ Code.cs
@@ -21,7 +21,7 @@ namespace EmployeeManagementSystem.Data
             this.WorkedHour = workedHour;
         }
 
-        [Key]
+  [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string EmployeeID { get; set; }
@@ -37,8 +37,33 @@ namespace EmployeeManagementSystem.Data
     }
 }
 
- public class EmployeeManagementContext : DbContext
-    {
-        public DbSet<Employee> Employees { get; set; }
-    }
+class Program 
+{ 
+    static void Main(string[] args) 
+    { 
+        using (var db = new BloggingContext()) 
+        { 
+            // Create and save a new Blog 
+            Console.Write("Enter a name for a new Blog: "); 
+            var name = Console.ReadLine(); 
+ 
+            var blog = new Blog { Name = name }; 
+            db.Blogs.Add(blog); 
+            db.SaveChanges(); 
+ 
+            // Display all Blogs from the database 
+            var query = from b in db.Blogs 
+                        orderby b.Name 
+                        select b; 
+ 
+            Console.WriteLine("All blogs in the database:"); 
+            foreach (var item in query) 
+            { 
+                Console.WriteLine(item.Name); 
+            } 
+ 
+            Console.WriteLine("Press any key to exit..."); 
+            Console.ReadKey(); 
+        } 
+    } 
 }
